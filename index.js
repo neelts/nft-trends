787d1b2fa10841177a62425ceedf7a90fee8cb09
neelts <neeelts@gmail.com>
2022-11-05 14:53:43 +0100

    Add delay between requests and retries if failed.

--- index.js
+++ index.js
@@ -13,22 +13,37 @@ const google_trends_api_1 = require("google-trends-api");
 const fs = require("fs");
 const period = (time) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        return JSON.parse(yield google_trends_api_1.interestOverTime(Object.assign({ keyword: 'NFT', timezone: 0, granularTimeResolution: true }, (time ? { startTime: new Date(Date.now() - time) } : null))));
+        return JSON.parse(yield (0, google_trends_api_1.interestOverTime)(Object.assign({ keyword: 'NFT', timezone: 0, granularTimeResolution: true }, (time ? { startTime: new Date(Date.now() - time) } : null))));
     }
     catch (e) {
         console.log(e);
     }
 });
+const sleep = () => new Promise(resolve => setTimeout(() => resolve(), 1000));
 const get = () => __awaiter(void 0, void 0, void 0, function* () {
     const day = 24 * 60 * 60 * 1000;
     const month = day * 30;
     const year = day * 355;
+    const request = (request) => __awaiter(void 0, void 0, void 0, function* () {
+        var _a;
+        let retries = 10;
+        do {
+            const data = yield request();
+            yield sleep();
+            console.log('>>> ' + retries);
+            if ((_a = data === null || data === void 0 ? void 0 : data.default) === null || _a === void 0 ? void 0 : _a.timelineData)
+                return data;
+            else
+                retries--;
+        } while (retries > 0);
+        return null;
+    });
     const transform = (data) => data.default.timelineData.map(value => [value.time, value.formattedTime, `${value.value[0]}`]);
-    const h = transform(yield period(day));
-    const d = transform(yield period(day * 7 - 10));
-    const w = transform(yield period(month * 9 - day));
-    const m = transform(yield period(year * 5 + month * 3 + day * 24));
-    const y = transform(yield period());
+    const h = transform(yield request(() => period(day)));
+    const d = transform(yield request(() => period(day * 7 - 10)));
+    const w = transform(yield request(() => period(month * 9 - day)));
+    const m = transform(yield request(() => period(year * 5 + month * 3 + day * 24)));
+    const y = transform(yield request(() => period()));
     fs.writeFileSync('trend.json', JSON.stringify({ h, d, w, m, y }, null, 2));
 });
 get();
--- index.ts
+++ index.ts
@@ -25,18 +25,38 @@ const period = async (time?: number): Promise<Data> => {
     }
 }
 
+const sleep = (): Promise<void> => new Promise<void>(resolve => setTimeout(() => resolve(), 1000));
+
 const get = async (): Promise<void> => {
+
+
+
     const day = 24 * 60 * 60 * 1000;
     const month = day * 30;
     const year = day * 355;
+
+    const request = async (request: () => Promise<Data>): Promise<Data> => {
+        let retries: number = 10;
+        do {
+            const data: Data = await request();
+            await sleep();
+            console.log('>>> ' + retries);
+            if (data?.default?.timelineData)
+                return data;
+            else
+                retries--;
+        } while (retries > 0);
+        return null;
+    };
+
     const transform = (data: Data): Record[] => data.default.timelineData.map(
         value => [value.time, value.formattedTime, `${value.value[0]}`]
     );
-    const h = transform(await period(day));
-    const d = transform(await period(day * 7 - 10));
-    const w = transform(await period(month * 9 - day));
-    const m = transform(await period(year * 5 + month * 3 + day * 24));
-    const y = transform(await period());
+    const h = transform(await request(() => period(day)));
+    const d = transform(await request(() => period(day * 7 - 10)));
+    const w = transform(await request(() => period(month * 9 - day)));
+    const m = transform(await request(() => period(year * 5 + month * 3 + day * 24)));
+    const y = transform(await request(() => period()));
     fs.writeFileSync('trend.json', JSON.stringify({h, d, w, m, y}, null, 2));
 };
 
